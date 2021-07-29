3c185a742cb73b60010feb4ae4170fd5f1afd4e8
Nzzi <cholnh1@naver.com>
2021-07-29 18:36:13 +0900

    sender exchange type - topic 변경

--- src/main/java/kr/nzzi/msa/mqtest2/event/sender/ProductSender.java
+++ src/main/java/kr/nzzi/msa/mqtest2/event/sender/ProductSender.java
@@ -2,18 +2,16 @@ package kr.nzzi.msa.mqtest2.event.sender;
 
 import kr.nzzi.msa.mqtest2.event.ProductEvent;
 import kr.nzzi.msa.mqtest2.rabbidmq.RabbitMqEvent;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class ProductSender implements CommandLineRunner {
 
     private final RabbitTemplate rabbitTemplate;
-
-    public ProductSender(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
 
     @Override
     public void run(String... args) {
@@ -22,10 +20,43 @@ public class ProductSender implements CommandLineRunner {
                 .price(3600)
                 .build();
 
-        System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(RabbitMqEvent.PRODUCT_MESSAGE_EVENT, "all message!!");
+        /* 브로드캐스트 */
+//        System.out.println("Sending message..." + RabbitMqEvent.PRODUCT_MESSAGE_BROADCAST_EVENT);
+//        rabbitTemplate.convertAndSend(
+//                RabbitMqEvent.PRODUCT_EXCHANGE,
+//                RabbitMqEvent.PRODUCT_MESSAGE_BROADCAST_EVENT,
+//                "all message!!");
+//
+//        System.out.println("Sending message..." + RabbitMqEvent.PRODUCT_SAVE_BROADCAST_EVENT);
+//        rabbitTemplate.convertAndSend(
+//                RabbitMqEvent.PRODUCT_EXCHANGE,
+//                RabbitMqEvent.PRODUCT_SAVE_BROADCAST_EVENT,
+//                event);
 
-        System.out.println("Sending my custom message");
-        rabbitTemplate.convertAndSend(RabbitMqEvent.PRODUCT_SAVE_EVENT, event);
+        /* 특정 타겟 전송 - monolith server */
+        System.out.println("Sending message..." + RabbitMqEvent.PRODUCT_MESSAGE_MONOLITHSERVER_EVENT);
+        rabbitTemplate.convertAndSend(
+                RabbitMqEvent.PRODUCT_EXCHANGE,
+                RabbitMqEvent.PRODUCT_MESSAGE_MONOLITHSERVER_EVENT,
+                "to monolith");
+
+        System.out.println("Sending message..." + RabbitMqEvent.PRODUCT_SAVE_MONOLITHSERVER_EVENT);
+        rabbitTemplate.convertAndSend(
+                RabbitMqEvent.PRODUCT_EXCHANGE,
+                RabbitMqEvent.PRODUCT_SAVE_MONOLITHSERVER_EVENT,
+                event);
+
+        /* 특정 타겟 전송 - mqtest1 */
+//        System.out.println("Sending message..." + RabbitMqEvent.PRODUCT_MESSAGE_MQTEST1_EVENT);
+//        rabbitTemplate.convertAndSend(
+//                RabbitMqEvent.PRODUCT_EXCHANGE,
+//                RabbitMqEvent.PRODUCT_MESSAGE_MQTEST1_EVENT,
+//                "to mqtest1");
+//
+//        System.out.println("Sending message..." + RabbitMqEvent.PRODUCT_SAVE_MQTEST1_EVENT);
+//        rabbitTemplate.convertAndSend(
+//                RabbitMqEvent.PRODUCT_EXCHANGE,
+//                RabbitMqEvent.PRODUCT_SAVE_MQTEST1_EVENT,
+//                event);
     }
 }
--- src/main/java/kr/nzzi/msa/mqtest2/rabbidmq/RabbitMqEvent.java
+++ src/main/java/kr/nzzi/msa/mqtest2/rabbidmq/RabbitMqEvent.java
@@ -1,6 +1,17 @@
 package kr.nzzi.msa.mqtest2.rabbidmq;
 
 public interface RabbitMqEvent {
-    String PRODUCT_MESSAGE_EVENT = "product.message";
-    String PRODUCT_SAVE_EVENT = "product.save";
+    String PRODUCT_EXCHANGE = "product.exchange";
+
+    /* 브로드캐스트 */
+    String PRODUCT_ALL_BROADCAST_EVENT = "product.#";
+    String PRODUCT_MESSAGE_BROADCAST_EVENT = "product.message.*";
+    String PRODUCT_SAVE_BROADCAST_EVENT = "product.save.*";
+
+    /* 특정 타겟 전송 */
+    String PRODUCT_MESSAGE_MONOLITHSERVER_EVENT = "product.message.monolithserver";
+    String PRODUCT_SAVE_MONOLITHSERVER_EVENT = "product.save.monolithserver";
+
+    String PRODUCT_MESSAGE_MQTEST1_EVENT = "product.message.mqtest1";
+    String PRODUCT_SAVE_MQTEST1_EVENT = "product.save.mqtest1";
 }
